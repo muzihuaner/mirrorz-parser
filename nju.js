530ab3056ad5fa4791eca7e3350ce8b6e9d408b8
Zenithal <i@zenithal.me>
2021-08-14 11:27:52 +0800

    Add info for nju

--- nju.js
+++ nju.js
@@ -1,13 +1,15 @@
 const tunasync = require("./tunasync");
 const options = require("./options");
+const isoinfo = require("./isoinfo");
 
 module.exports = async function () {
   const site = await (await fetch("https://mirrors.nju.edu.cn/.mirrorz/site.json")).json();
   const mirrors = await tunasync("https://mirrors.nju.edu.cn/.mirrorz/tunasync.json");
+  const info = await isoinfo("https://mirror.nju.edu.cn/.mirrorz/iso.json");
 
   return {
     site,
-    info: [],
+    info,
     mirrors,
   }
 };
