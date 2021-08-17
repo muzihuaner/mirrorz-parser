5b41bb84bca571163de3ed10847a5130b16bef7e
Zenithal <i@zenithal.me>
2021-08-17 15:10:53 +0800

    Use desc_help as mirrors for bjtu

--- bjtu.js
+++ bjtu.js
@@ -1,20 +1,33 @@
+const cname = require("./utils").cname;
 const ideal_mirror = require("./ideal-mirror");
 
 module.exports = async function (siteUrl) {
+  const name_func = await cname();
   const site = await (await fetch(siteUrl)).json();
-  const mirrors = await ideal_mirror("https://mirror.bjtu.edu.cn/status/task_status.json");
+  const ideal = await ideal_mirror("https://mirror.bjtu.edu.cn/status/task_status.json");
   const desc_help = await (await fetch("https://mirror.bjtu.edu.cn/help/desc_help.json")).json();
 
+  const mirrors = []
   for (const d in desc_help) {
     const p = desc_help[d];
-    for (const m of mirrors) {
-      if (m.cname == d) {
+    const c = name_func(d);
+    for (const m of ideal) {
+      if (m.cname == c) {
+        const q = {
+          cname: c,
+        }
         if ("link" in p)
-          m.url = p.link;
+          q.url = p.link;
         if ("desc" in p)
-          m.desc = p.desc;
+          q.desc = p.desc;
         if ("help" in p)
-          m.help = p.help;
+          q.help = p.help;
+        if ("upstream" in m)
+          q.upstream = m.upstream;
+        q.status = m.status;
+        if ("size" in m)
+          q.size = m.size;
+        mirrors.push(q);
       }
     }
   }
