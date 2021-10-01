2c4a6854c39c4599352b817b2d3bf198e1ae84b9
Shiwei Wang <38160380+AirWSW@users.noreply.github.com>
2021-10-01 12:35:55 +0800

    Update xjtu.js
    add trailing slash at the end of URL, fix https://github.com/mirrorz-org/mirrorz/issues/70

--- xjtu.js
+++ xjtu.js
@@ -2,7 +2,8 @@ const tunasync = require("./tunasync");
 
 module.exports = async function (siteUrl) {
   const site = await (await fetch(siteUrl)).json();
-  const mirrors = await tunasync("https://mirrors.xjtu.edu.cn/api/status.json");
+  let mirrors = await tunasync("https://mirrors.xjtu.edu.cn/api/status.json");
+  mirrors = mirrors.map((item) => ({ ...item, url: item.url + "/" }));
 
   return {
     site,
