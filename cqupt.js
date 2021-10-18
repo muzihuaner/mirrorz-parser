787549a9ac40e8c6c4418925625ed3503c23328f
ZenithalHourlyRate <i@zenithal.me>
2021-10-18 20:56:29 +0800

    cqupt: change url to edu.cn

--- cqupt.js
+++ cqupt.js
@@ -2,7 +2,7 @@ const tunasync = require("./tunasync");
 
 module.exports = async function (siteUrl) {
   const site = await (await fetch(siteUrl)).json();
-  const mirrors = await tunasync("https://mirrors.redrock.team/static/tunasync.json");
+  const mirrors = await tunasync("https://mirrors.cqupt.edu.cn/static/tunasync.json");
 
   return {
     site,
