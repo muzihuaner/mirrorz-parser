c5a2667a7c5994506fbdebe9dbbce18abe8f60c9
Zenithal <i@zenithal.me>
2023-01-20 20:24:59 +0800

    lint: sort to stablize legacy-json

--- lint.js
+++ lint.js
@@ -11,12 +11,16 @@ module.exports = async function (mirrorz) {
     // (as mirrorz.json directly from mirror sites may not get the latest cname.json)
     mirror.cname = name_func(mirror.cname);
   }
+  // sort by mirrors.cname
+  mirrorz.mirrors = mirrorz.mirrors.sort((l, r) => l.cname.localeCompare(r.cname))
   // re-apply cname for info.distro
   // (as mirrorz.json directly from mirror sites may not get the latest cname.json)
   if ("info" in mirrorz) {
     for (let info of mirrorz.info) {
       info.distro = name_func(info.distro);
     }
+    // sort by info.distro
+    mirrorz.info = mirrorz.info.sort((l, r) => l.distro.localeCompare(r.distro))
   }
   return mirrorz
 };
