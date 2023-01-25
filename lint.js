e93ee58d6db72404d467114edffdb00d284085ca
Zenithal <i@zenithal.me>
2023-01-25 14:43:47 +0800

    sjtug: fix pypi/nix-channels url

--- lint.js
+++ lint.js
@@ -1,5 +1,20 @@
 const cname = require("./utils").cname;
 
+// quirks for some site
+function quirks(mirrorz) {
+  if (mirrorz.site.abbr.includes("SJTUG")) {
+    for (let mirror of mirrorz.mirrors) {
+      if (mirror.url === "/pypi/web/simple") {
+        mirror.url = "/pypi";
+      }
+      if (mirror.url === "/nix-channels/store") {
+        mirror.url = "/nix-channels";
+      }
+    }
+  }
+  return mirrorz;
+}
+
 module.exports = async function (mirrorz) {
   const name_func = await cname();
   // remove trailing slash in site.url
@@ -22,5 +37,5 @@ module.exports = async function (mirrorz) {
     // sort by info.distro
     mirrorz.info = mirrorz.info.sort((l, r) => l.distro.localeCompare(r.distro))
   }
-  return mirrorz
+  return quirks(mirrorz)
 };
