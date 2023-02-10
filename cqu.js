7a6bb5723af8873f04068425e7a9179c18c9ce34
Zenithal <i@zenithal.me>
2023-02-11 00:56:28 +0800

    cqu: no hack for crates.io now
    See https://github.com/mirrorz-org/mirrorz/commit/c7b8e9ada7a7df82bc64824eaa3324400b392fe3
    See https://github.com/mirrorz-org/mirrorz-help/commit/53cfc164ce340f1cffac2df078db2fe0dafcb5df

--- cqu.js
+++ cqu.js
@@ -7,11 +7,7 @@ module.exports = async function (siteUrl) {
   const info = await isoinfo("https://mirrors.cqu.edu.cn/static/isoinfo.json");
 
   for (const m of mirrors) {
-    // cqu has crates.io-index, and it is cname'd to .git
-    // but its url is not updated
-    if (m.cname === "crates.io-index.git") {
-      m.url = '/git/crates.io-index.git'
-    } else if (m.cname.includes(".git")) {
+    if (m.cname.includes(".git")) {
       // a hack for cqu, they put .git in /git/xxx.git
       m.url = '/git' + m.url
     }
