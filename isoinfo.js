2deb115895510bedc4fc3ef6a8c005c881c71055
taoky <me@taoky.moe>
2024-03-07 00:12:59 +0800

    isoinfo: allow empty "JSON" isoinfo files
    Though this usually indicates a bug with mirror sites' isoinfo generation scripts.

--- isoinfo.js
+++ isoinfo.js
@@ -2,7 +2,15 @@ const cname = require("./utils").cname;
 
 module.exports = async function (isoinfoUrl) {
   const name_func = await cname();
-  const isoinfo = await (await fetch(isoinfoUrl)).json();
+  const isoinfo = await fetch(isoinfoUrl)
+    .then((res) => res.text())
+    .then((text) => {
+      if (text.trim() === "") {
+        return [];
+      } else {
+        return JSON.parse(text);
+      }
+    });
 
   return isoinfo.map((item) => {
     return {
