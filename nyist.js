c4bc45b487ff9cc81e393e6984a12c54b685affb
Palve <3315307975@qq.com>
2024-03-06 18:32:09 +0800

    nyist: Add config support for siteUrl (#9)
    * Add config support for siteUrl
    
    * format

--- nyist.js
+++ nyist.js
@@ -1,12 +1,16 @@
 const tunasync = require("./tunasync");
+const options = require("./options");
+const isoinfo = require("./isoinfo");
 
 module.exports = async function () {
   const site = await (await fetch("https://mirror.nyist.edu.cn/static/site.json")).json();
-  const mirrors = await tunasync("https://mirror.nyist.edu.cn/static/tunasync.json");
-
+  let mirrors = await tunasync("https://mirror.nyist.edu.cn/static/tunasync.json");
+  mirrors = await options("https://mirror.nyist.edu.cn/static/js/options.json", mirrors);
+  const info = await isoinfo("https://mirror.nyist.edu.cn/static/status/isoinfo.json");
+  
   return {
     site,
-    info: [],
+    info,
     mirrors,
   }
 };
