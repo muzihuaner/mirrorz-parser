ea9726b8d12caeadc738dcae512e9b85a150bdb7
iBug <git@ibugone.com>
2024-06-08 01:53:00 +0800

    utils.js: Add caching for fetch()

--- utils.js
+++ utils.js
@@ -1,6 +1,10 @@
-const config = require('./config.json')
+const config = require('./config.json');
+var cname;
 
 exports.cname = async function() {
-  const cname = await (await fetch(config.url + "/static/json/cname.json")).json();
+  if (!cname) {
+    data = await fetch(config.url + "/static/json/cname.json");
+    cname = await data.json();
+  }
   return (name) => { return (name in cname) ? cname[name] : name; };
 };
