67d043fa3e65e5e9eb678e4c99f5ab139d5b4d14
taoky <me@taoky.moe>
2024-06-17 05:03:58 +0800

    utils.js: Make JavaScript transpiler happy

--- utils.js
+++ utils.js
@@ -2,6 +2,10 @@ const config = require('./config.json');
 var cname;
 
 exports.cname = async function() {
+  // Make JavaScript transpiler happy
+  // Otherwise it would compile incorrect code and thus making "ReferenceError: data is not defined" error.
+  let data;
+
   if (!cname) {
     data = await fetch(config.url + "/static/json/cname.json");
     cname = await data.json();
