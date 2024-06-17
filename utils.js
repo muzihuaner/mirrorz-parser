df52964a10ca367a47e941a953b1fa791607516b
taoky <me@taoky.moe>
2024-06-17 15:13:34 +0800

    utils.js: Use const for data

--- utils.js
+++ utils.js
@@ -2,12 +2,8 @@ const config = require('./config.json');
 var cname;
 
 exports.cname = async function() {
-  // Make JavaScript transpiler happy
-  // Otherwise it would compile incorrect code and thus making "ReferenceError: data is not defined" error.
-  let data;
-
   if (!cname) {
-    data = await fetch(config.url + "/static/json/cname.json");
+    const data = await fetch(config.url + "/static/json/cname.json");
     cname = await data.json();
   }
   return (name) => { return (name in cname) ? cname[name] : name; };
