da04d283af0070cc0b4f204376d83780973d5592
木子欢儿～ <2209514944@qq.com>
2024-08-05 15:10:38 +0800

    Update nwafu.js

--- nwafu.js
+++ nwafu.js
@@ -2,7 +2,7 @@ const tunasync = require("./tunasync");
 
 module.exports = async function (siteUrl) {
   const site = await (await fetch(siteUrl)).json();
-  const mirrors = await tunasync("https://mirrors.nwafu.edu.cn/api/mirrorz/jobs");
+  const mirrors = await tunasync("https://cors.quickso.cn/?https://mirrors.nwafu.edu.cn/api/mirrorz/jobs");
 
   return {
     site,
