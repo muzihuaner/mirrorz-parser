bc82fcfec0d3cf60f89d9bc7a4cd0516e52c7f5e
木子欢儿～ <2209514944@qq.com>
2024-08-05 15:10:13 +0800

    Update jcut.js

--- jcut.js
+++ jcut.js
@@ -2,7 +2,7 @@ const tunasync = require("./tunasync");
 
 module.exports = async function (siteUrl) {
   const site = await (await fetch(siteUrl)).json();
-  const mirrors = await tunasync("https://mirrors.jcut.edu.cn/jobs");
+  const mirrors = await tunasync("https://cors.quickso.cn/?https://mirrors.jcut.edu.cn/jobs");
 
   return {
       site,
