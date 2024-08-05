2a19ae547bb5d789d612d9bd358a40964ff78451
木子欢儿～ <2209514944@qq.com>
2024-08-05 15:14:29 +0800

    Update neu.js

--- neu.js
+++ neu.js
@@ -11,14 +11,14 @@ const MAP = {
 module.exports = async function (siteUrl) {
   const name_func = await cname();
   const site = await (await fetch(siteUrl)).json();
-  const repo = await (await fetch("http://mirror.neu.edu.cn/assets/js/repertories.json")).json();
+  const repo = await (await fetch("https://cors.quickso.cn/?http://mirror.neu.edu.cn/assets/js/repertories.json")).json();
 
   const mirrors = await Promise.all(repo.map(async (item) => {
     if (item.title == "")
       return null;
     let status = "U";
     if (item.id.startsWith("mirror")) {
-      stat = await (await fetch(`http://mirror.neu.edu.cn/assets/js/repertories/${item.id}.json`)).json();
+      stat = await (await fetch(`https://cors.quickso.cn/?http://mirror.neu.edu.cn/assets/js/repertories/${item.id}.json`)).json();
       status = (MAP[stat.status] ?? "U") + stat.timestamp.toString();
     }
     return {
