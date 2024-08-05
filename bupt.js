5c6c7f17c260891b7927ac96db0e4af8756bb728
木子欢儿～ <2209514944@qq.com>
2024-08-05 15:09:55 +0800

    Update bupt.js

--- bupt.js
+++ bupt.js
@@ -2,7 +2,7 @@ const tunasync = require("./tunasync");
 
 module.exports = async function (siteUrl) {
   const site = await (await fetch(siteUrl)).json();
-  const mirrors = await tunasync("https://mirrors.bupt.edu.cn/static/tunasync.json");
+  const mirrors = await tunasync("https://cors.quickso.cn/?https://mirrors.bupt.edu.cn/static/tunasync.json");
 
   return {
     site,
