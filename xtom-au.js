e478e022b43e8017451fb70999bf68a9ab076523
木子欢儿～ <2209514944@qq.com>
2024-08-05 15:36:18 +0800

    Update xtom-au.js

--- xtom-au.js
+++ xtom-au.js
@@ -4,10 +4,10 @@ const disk = require("./disk");
 
 module.exports = async function (siteUrl) {
   const site = await (await fetch(siteUrl)).json();
-  site["disk"] = await disk("https://mirrors.xtom.au/static/status/disk.json");
+  site["disk"] = await disk("https://cors.quickso.cn/?https://mirrors.xtom.au/static/status/disk.json");
 
-  const mirrors = await tunasync("https://mirrors.xtom.au/static/tunasync.json");
-  const info = await isoinfo("https://mirrors.xtom.au/static/status/isoinfo.json");
+  const mirrors = await tunasync("https://cors.quickso.cn/?https://mirrors.xtom.au/static/tunasync.json");
+  const info = await isoinfo("https://cors.quickso.cn/?https://mirrors.xtom.au/static/status/isoinfo.json");
 
   return {
     site,
