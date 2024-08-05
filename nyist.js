9ada71629066f7db9d81f600f8106bcc7800f723
木子欢儿～ <2209514944@qq.com>
2024-08-05 15:05:12 +0800

    Update nyist.js

--- nyist.js
+++ nyist.js
@@ -3,10 +3,10 @@ const options = require("./options");
 const isoinfo = require("./isoinfo");
 
 module.exports = async function () {
-  const site = await (await fetch("https://mirror.nyist.edu.cn/static/site.json")).json();
-  let mirrors = await tunasync("https://mirror.nyist.edu.cn/static/tunasync.json");
-  mirrors = await options("https://mirror.nyist.edu.cn/static/js/options.json", mirrors);
-  const info = await isoinfo("https://mirror.nyist.edu.cn/static/status/isoinfo.json");
+  const site = await (await fetch("https://cors.quickso.cn/?https://mirror.nyist.edu.cn/static/site.json")).json();
+  let mirrors = await tunasync("https://cors.quickso.cn/?https://mirror.nyist.edu.cn/static/tunasync.json");
+  mirrors = await options("https://cors.quickso.cn/?https://mirror.nyist.edu.cn/static/js/options.json", mirrors);
+  const info = await isoinfo("https://cors.quickso.cn/?https://mirror.nyist.edu.cn/static/status/isoinfo.json");
   
   return {
     site,
