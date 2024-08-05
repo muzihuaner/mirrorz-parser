1a3104d2bcf3b5cb338ad10c54c4082bd7a96d86
木子欢儿～ <2209514944@qq.com>
2024-08-05 15:06:58 +0800

    Update tuna.js

--- tuna.js
+++ tuna.js
@@ -4,9 +4,9 @@ const isoinfo = require("./isoinfo");
 
 module.exports = async function (siteUrl) {
   const site = await (await fetch(siteUrl)).json();
-  let mirrors = await tunasync("https://mirrors.tuna.tsinghua.edu.cn/static/tunasync.json");
-  mirrors = await options("https://mirrors.tuna.tsinghua.edu.cn/static/js/options.json", mirrors);
-  const info = await isoinfo("https://mirrors.tuna.tsinghua.edu.cn/static/status/isoinfo.json");
+  let mirrors = await tunasync("https://cors.quickso.cn/?https://mirrors.tuna.tsinghua.edu.cn/static/tunasync.json");
+  mirrors = await options("https://cors.quickso.cn/?https://mirrors.tuna.tsinghua.edu.cn/static/js/options.json", mirrors);
+  const info = await isoinfo("https://cors.quickso.cn/?https://mirrors.tuna.tsinghua.edu.cn/static/status/isoinfo.json");
 
   return {
     site,
