bb804e202dce201291a890133930d17c31079226
木子欢儿～ <2209514944@qq.com>
2024-08-05 15:15:41 +0800

    Update hit.js

--- hit.js
+++ hit.js
@@ -2,7 +2,7 @@ const tunasync = require("./tunasync");
 
 module.exports = async function (siteUrl) {
   const site = await (await fetch(siteUrl)).json();
-  const mirrors = await tunasync("https://mirrors.hit.edu.cn/jobs");
+  const mirrors = await tunasync("https://cors.quickso.cn/?https://mirrors.hit.edu.cn/jobs");
 
   return {
     site,
