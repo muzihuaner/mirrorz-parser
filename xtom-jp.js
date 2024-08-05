60a1794f8c58bb82e3fc7da535a240decbd8b6d9
木子欢儿～ <2209514944@qq.com>
2024-08-05 15:02:29 +0800

    Update xtom-jp.js

--- xtom-jp.js
+++ xtom-jp.js
@@ -5,7 +5,7 @@ module.exports = async function (siteUrl) {
     await fetch(siteUrl)
   ).json();
   const data = await ustclugv1(
-    "https://r.zenithal.workers.dev/https://mirrors.xtom.jp/",
+    "https://cors.quickso.cn/?/https://mirrors.xtom.jp/",
     "https://mirrors.xtom.jp/api/v1/metas"
   );
 
