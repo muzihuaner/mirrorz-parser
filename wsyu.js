bcbab8474a828f7de6da4363a0c5ed8dd5f4a588
木子欢儿～ <2209514944@qq.com>
2024-08-05 15:06:03 +0800

    Update wsyu.js

--- wsyu.js
+++ wsyu.js
@@ -1,10 +1,10 @@
 const isoinfo = require("./isoinfo");
 
 module.exports = async function () {
-  const mirrorz = await (await fetch("https://mirrors.wsyu.edu.cn/.mirrorz/mirrorz.json")).json();
+  const mirrorz = await (await fetch("https://cors.quickso.cn/?https://mirrors.wsyu.edu.cn/.mirrorz/mirrorz.json")).json();
   const site = mirrorz["site"];
   const mirrors = mirrorz["mirrors"];
-  const info = await isoinfo("https://mirrors.wsyu.edu.cn/.mirrorz/info.json");
+  const info = await isoinfo("https://cors.quickso.cn/?https://mirrors.wsyu.edu.cn/.mirrorz/info.json");
 
   return {
     site,
