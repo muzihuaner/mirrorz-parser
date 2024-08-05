1722a0402cb12ce8ef075aa6dfd5ab2894985104
木子欢儿～ <2209514944@qq.com>
2024-08-05 15:02:42 +0800

    Update xtom-hk.js

--- xtom-hk.js
+++ xtom-hk.js
@@ -5,7 +5,7 @@ module.exports = async function (siteUrl) {
     await fetch(siteUrl)
   ).json();
   const data = await ustclugv1(
-    "https://r.zenithal.workers.dev/https://mirrors.xtom.com.hk/",
+    "https://cors.quickso.cn/?https://mirrors.xtom.com.hk/",
     "https://mirrors.xtom.com.hk/api/v1/metas"
   );
 
